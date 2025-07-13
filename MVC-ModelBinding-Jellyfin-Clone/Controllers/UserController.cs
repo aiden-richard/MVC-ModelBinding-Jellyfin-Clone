17ca640863cbe709952418de901b2546ee625807
aiden-richard <richardaiden2006@gmail.com>
2025-07-12 19:58:15 -0700

    Create User controller and add an action called Create

---
+++ MVC-ModelBinding-Jellyfin-Clone/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_ModelBinding_Jellyfin_Clone.Controllers;
+
+public class UserController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
