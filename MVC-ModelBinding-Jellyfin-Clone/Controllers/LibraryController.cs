ba010635bc6de1d6b65dbbb2f515a1f875fb54ab
aiden-richard <richardaiden2006@gmail.com>
2025-07-14 12:29:50 -0700

    Add LibraryController and a Create action

---
+++ MVC-ModelBinding-Jellyfin-Clone/Controllers/LibraryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_ModelBinding_Jellyfin_Clone.Controllers;
+
+public class LibraryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
