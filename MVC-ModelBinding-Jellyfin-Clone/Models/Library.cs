8e3eed119e6171a76ca9d78c594050306e092857
aiden-richard <richardaiden2006@gmail.com>
2025-07-14 12:25:21 -0700

    Add Library class that represents a library. Has Id, ContentType, DisplayName, and Directory fields

---
+++ MVC-ModelBinding-Jellyfin-Clone/Models/Library.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_ModelBinding_Jellyfin_Clone.Models;
+
+public class Library
+{
+    /// <summary>
+    /// A unique int32 identifier for the library
+    /// This is the library's primary key in the database
+    /// Should be set by the database
+    /// </summary>
+    [Key]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The ContentType of the library, stored as a string
+    /// Valid values are "Movies", "Music", "Books", and "Shows"
+    /// </summary>
+    [RegularExpression(@"^(Movies|Music|Books|Shows)$", ErrorMessage = "Content Type Must be one of (\"Movies\", \"Music\", \"Books\", and \"Shows\")")]
+    public string ContentType { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The name of the library, stored as a string
+    /// Valid characters are A-Z, a-z, 0-9, and underscores
+    /// </summary>
+    [StringLength(50, MinimumLength = 1, ErrorMessage = "Library name must not be longer than 50 characters")]
+    [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Library name can only contain letters, numbers, and underscores")]
+    public string DisplayName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The directory where the library is stored, stored as a string
+    /// Must start with a forward slash (/)
+    /// </summary>
+    [RegularExpression(@"^\/.*", ErrorMessage = "Directory must start with a forward slash (/)")]
+    public string Directory { get; set; } = string.Empty;
+}
