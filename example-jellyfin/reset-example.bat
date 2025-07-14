ddbeddb986c85b08111d8a974aa808707843506d
Aiden Richard <115966042+aiden-richard@users.noreply.github.com>
2025-07-14 00:51:26 -0700

    Update reset-example.bat
    Change AI generated comments for clarity

--- example-jellyfin/reset-example.bat
+++ example-jellyfin/reset-example.bat
@@ -1,8 +1,8 @@
 @echo off
-:: Delete contents of Folder 1
+:: Delete contents of Folder config
 del /q ".\config\*.*"
 for /d %%i in (".\config\*") do rd /s /q "%%i"
 
-:: Delete contents of Folder 2
+:: Delete contents of Folder cache
 del /q ".\cache\*.*"
 for /d %%i in (".\cache\*") do rd /s /q "%%i"
