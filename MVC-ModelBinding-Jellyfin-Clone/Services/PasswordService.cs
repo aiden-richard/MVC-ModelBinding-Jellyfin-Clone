80772ab24a7a6eb632ce2ddc27fdb716a9a78001
aiden-richard <richardaiden2006@gmail.com>
2025-07-14 00:00:19 -0700

    Add PasswordService Class for hashing user passwords with HMACSHA512

---
+++ MVC-ModelBinding-Jellyfin-Clone/Services/PasswordService.cs
@@ -0,0 +1,27 @@
+﻿namespace MVC_ModelBinding_Jellyfin_Clone.Services;
+
+/// <summary>
+/// This service is used to create a password hash and salt
+/// </summary>
+public class PasswordService
+{
+    /// <summary>
+    /// This method creates a password hash and salt
+    /// and stores them in the provided byte arrays.
+    /// </summary>
+    /// <param name="password">The user's plaintext password</param>
+    /// <param name="passwordHash">The byte array that will hold the hash</param>
+    /// <param name="passwordSalt">The byte array that will hold the salt</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
+            throw new ArgumentNullException(nameof(password), "Password cannot be null or empty");
+        }
+
+        using var hmac = new System.Security.Cryptography.HMACSHA512();
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+    }
+}
