f7ea1c3870b200555d37549cdbe379939a3a1cb1
aiden-richard <richardaiden2006@gmail.com>
2025-07-14 00:07:47 -0700

    Implement PasswordService in UserControllet. Takes plaintext password from CreateUserViewModel and Hashes it before creating a User object with the new hash and salt

--- MVC-ModelBinding-Jellyfin-Clone/Controllers/UserController.cs
+++ MVC-ModelBinding-Jellyfin-Clone/Controllers/UserController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVC_ModelBinding_Jellyfin_Clone.Models;
+using MVC_ModelBinding_Jellyfin_Clone.Services;
 
 namespace MVC_ModelBinding_Jellyfin_Clone.Controllers;
 
-public class UserController : Controller
+public class UserController(PasswordService passwordService) : Controller
 {
+    private readonly PasswordService PasswordService = passwordService;
+
     public IActionResult Index()
     {
         return View();
@@ -21,6 +24,11 @@ public class UserController : Controller
     {
         if (ModelState.IsValid)
         {
+            // Hash the password
+            PasswordService.CreatePasswordHash(new_user.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            
+            User user = new(new_user.Username, passwordHash, passwordSalt, new_user.Role, new_user.PreferredLanguage);
+
             // Add the user to the database
 
             // Redirect to the Users page
