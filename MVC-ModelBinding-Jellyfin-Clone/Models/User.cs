22d707a232d9f81347def32221e49df59a6b1b10
aiden-richard <richardaiden2006@gmail.com>
2025-07-13 23:55:03 -0700

    Change User Class to use a primary constructor. Remove data annotations because those are handled by CreateUserViewModel which. Remove required keyword from Username and PreferredLanguage because it was causing error CS9035

--- MVC-ModelBinding-Jellyfin-Clone/Models/User.cs
+++ MVC-ModelBinding-Jellyfin-Clone/Models/User.cs
@@ -5,16 +5,8 @@ namespace MVC_ModelBinding_Jellyfin_Clone.Models;
 /// <summary>
 /// Represents a user in the jellyfin server
 /// </summary>
-public class User
+public class User(string username, byte[] passwordHash, byte[] passwordSalt, string role, string preferredLanguage)
 {
-    public User(string _Username, byte[] _PasswordHash, byte[] _PasswordSalt, string _PrefferedLanguage)
-    {
-        Username = _Username;
-        PasswordHash = _PasswordHash;
-        PasswordSalt = _PasswordSalt;
-        PreferredLanguage = _PrefferedLanguage;
-    }
-
     /// <summary>
     /// A unique int32 identifier for the user
     /// This is the user's primary key in the database
@@ -27,20 +19,17 @@ public class User
     /// The user's username, stored as a string
     /// Valid characters are A-Z, a-z, 0-9, and underscores
     /// </summary>
-    [StringLength(50, MinimumLength = 1)]
-    [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Username can only contain letters, numbers, and underscores")]
-    [Required(ErrorMessage = "Username is required")]
-    public required string Username { get; set; }
+    public string Username { get; set; } = username;
 
     /// <summary>
     /// The user's password hash, stored as a byte array
     /// </summary>
-    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordHash { get; set; } = passwordHash;
 
     /// <summary>
     /// The user's password salt, stored as a byte array
     /// </summary>
-    public byte[] PasswordSalt { get; set; }
+    public byte[] PasswordSalt { get; set; } = passwordSalt;
 
     /// <summary>
     /// Role of the user, stored as a string
@@ -49,14 +38,10 @@ public class User
     /// <remarks>
     /// If more work is put into this project, consider using ASP.NET Identity for built-in role and claim management.
     /// </remarks>
-    [Required(ErrorMessage = "Role is required")]
-    [RegularExpression(@"^(Admin|User)$", ErrorMessage = "Role must be either 'Admin' or 'User'")]
-    public string Role { get; set; } = "User";
+    public string Role { get; set; } = role;
 
     /// <summary>
     /// Preffered language for the user, stored as a string
     /// </summary>
-    [Required(ErrorMessage = "Preferred language is required")]
-    [StringLength(50, ErrorMessage = "Language name can't be longer than 50 characters")]
-    public required string PreferredLanguage { get; set; } = "English";
+    public string PreferredLanguage { get; set; } = preferredLanguage;
 }
